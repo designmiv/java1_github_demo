d88e49a155b013b59c3d0aac6b3fea88b18919c7
mventimiglia7 <mventimiglia7@MCBA202-C14.classrooms.stlcc>
2017-09-20 11:01:10 -0500

    first commit

---
+++ src/gitHubDemo/Main_Demo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitHubDemo;
+
+public class Main_Demo {
+    // psvm shortcut
+    public static void main(String[] args) {
+        // sout shortcut
+        System.out.println("Hello, world!");
+    }
+}
